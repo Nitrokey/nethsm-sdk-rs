5cce31933b0e08a4b2e9d779dfdd7c3f110e861d
Robin Krahl <robin@nitrokey.com>
2024-05-15 19:01:42 +0200

    Include API spec in repository
    Including the API spec makes it possible to reproducibly re-generate the
    client code and to select the exact spec version that is used for the
    generation.
    
    Fixes: https://github.com/Nitrokey/nethsm-sdk-rs/issues/23

---
+++ Makefile
@@ -0,0 +1,2 @@
+nethsm-api.yaml:
+	curl "https://nethsmdemo.nitrokey.com/api_docs/nethsm-api.yaml" --output nethsm-api.yaml
--- update_openapi.sh
+++ update_openapi.sh
@@ -2,5 +2,5 @@
 
 docker build -t crust:latest generator
 
-docker run --rm -u $UID -v "${PWD}:/out" -v "${PWD}/generator_conf.yaml:/conf.yaml" crust:latest generate -i=https://nethsmdemo.nitrokey.com/api_docs/nethsm-api.yaml -o out -g crust -c /conf.yaml
+docker run --rm -v "${PWD}:/out" -v "${PWD}/nethsm-api.yaml:/nethsm-api.yaml" -v "${PWD}/generator_conf.yaml:/conf.yaml" crust:latest generate -i=/nethsm-api.yaml -o out -g crust -c /conf.yaml
 cargo fmt
