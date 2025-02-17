4f5201d77f2b21bb54e2fc183e02b504b8e7c54f
Robin Krahl <robin@nitrokey.com>
2025-02-17 16:15:20 +0100

    Remove clippy exception for result_large_err
    The lint has been fixed by updating to ureq v3 so we no longer need an
    exception for it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-// ureq v2 has a large error type, thus our errors are large too.
-// This will be fixed by updating to ureq v3.
-#![allow(clippy::result_large_err)]
-
 #[macro_use]
 extern crate serde_derive;
 
